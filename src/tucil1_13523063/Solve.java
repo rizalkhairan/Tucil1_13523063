fe3967425ad1e2acc7f085a66f771d96ae4aadff
Syahrizal Khairan <13523063@std.stei.itb.ac.id>
2025-02-23 00:45:51 +0700

    fix: correctly recurse into 3D orientations instead of 2D

--- src/tucil1_13523063/Solve.java
+++ src/tucil1_13523063/Solve.java
@@ -7,7 +7,7 @@ public class Solve {
         } else if (problem.mode.equals("PYRAMID")) {
             Solve.recurse_3D(problem, 0);
         } else {
-            System.out.println("Invalid board");
+            System.out.println("Invalid mode");
             return null;
         }
         
@@ -80,46 +80,67 @@ public class Solve {
             Coordinate coord = possibleCoordinates[i];            
 
             // Same thing as recurse_2D_branch, but with 3D orientations
-            // Not cartesian currently. See Coordinate.java
-            // recurse_3D_branch place the block, recurse, remove, then skew about vector i-j
+            // Not in cartesian coordinate system currently. See Coordinate.java
+            // recurse_3D_branch place the block, recurse, remove, then transform the block
             // Block is raised, rotated about the z-axis, and flattened in recurse_3D
             // There may be redundant cases, but i just don't want to think any more
             
             /* Planar */
             // Not flipped. All rotations
-            Solve.recurse_2D_branch(problem, blockIndex, coord);
-            Solve.recurse_2D_branch(problem, blockIndex, coord);
-            Solve.recurse_2D_branch(problem, blockIndex, coord);
-            Solve.recurse_2D_branch(problem, blockIndex, coord);
+            Solve.recurse_3D_branch(problem, blockIndex, coord, 0);
+            Solve.recurse_3D_branch(problem, blockIndex, coord, 0);
+            Solve.recurse_3D_branch(problem, blockIndex, coord, 0);
+            Solve.recurse_3D_branch(problem, blockIndex, coord, 0);
     
             // Horizontally flipped. All rotations
             if (!problem.solved()) {
                 currentBlock.reflect_horizontal();
-                Solve.recurse_2D_branch(problem, blockIndex, coord);
-                Solve.recurse_2D_branch(problem, blockIndex, coord);
-                Solve.recurse_2D_branch(problem, blockIndex, coord);
-                Solve.recurse_2D_branch(problem, blockIndex, coord);
+                Solve.recurse_3D_branch(problem, blockIndex, coord, 0);
+                Solve.recurse_3D_branch(problem, blockIndex, coord, 0);
+                Solve.recurse_3D_branch(problem, blockIndex, coord, 0);
+                Solve.recurse_3D_branch(problem, blockIndex, coord, 0);
             }
             
-            /* 3D orientations */
+            /* 3D */
             if (!problem.solved()) {
+                currentBlock.reflect_horizontal();
                 currentBlock.raise();
-                
-                Solve.recurse_3D_branch(problem, blockIndex, coord, true);
-                Solve.recurse_3D_branch(problem, blockIndex, coord, true);
-                Solve.recurse_3D_branch(problem, blockIndex, coord, true);
-                Solve.recurse_3D_branch(problem, blockIndex, coord, true);
 
+                // Normal vector is i-j
+                Solve.recurse_3D_branch(problem, blockIndex, coord, 1);
+                Solve.recurse_3D_branch(problem, blockIndex, coord, 1);
+                Solve.recurse_3D_branch(problem, blockIndex, coord, 1);
+                Solve.recurse_3D_branch(problem, blockIndex, coord, 1);
+
+                // Normal vector is -i-j
                 if (!problem.solved()) {
                     currentBlock.rotate_90_cw(1);
-                    Solve.recurse_3D_branch(problem, blockIndex, coord, true);
-                    Solve.recurse_3D_branch(problem, blockIndex, coord, true);
-                    Solve.recurse_3D_branch(problem, blockIndex, coord, true);
-                    Solve.recurse_3D_branch(problem, blockIndex, coord, true);
+                    Solve.recurse_3D_branch(problem, blockIndex, coord, 1);
+                    Solve.recurse_3D_branch(problem, blockIndex, coord, 1);
+                    Solve.recurse_3D_branch(problem, blockIndex, coord, 1);
+                    Solve.recurse_3D_branch(problem, blockIndex, coord, 1);
+                }
+
+                // Normal vector is -i+j
+                if (!problem.solved()) {
+                    currentBlock.rotate_90_cw(1);
+                    Solve.recurse_3D_branch(problem, blockIndex, coord, 1);
+                    Solve.recurse_3D_branch(problem, blockIndex, coord, 1);
+                    Solve.recurse_3D_branch(problem, blockIndex, coord, 1);
+                    Solve.recurse_3D_branch(problem, blockIndex, coord, 1);
+                }
+
+                // Normal vector is i+j
+                if (!problem.solved()) {
+                    currentBlock.rotate_90_cw(1);
+                    Solve.recurse_3D_branch(problem, blockIndex, coord, 1);
+                    Solve.recurse_3D_branch(problem, blockIndex, coord, 1);
+                    Solve.recurse_3D_branch(problem, blockIndex, coord, 1);
+                    Solve.recurse_3D_branch(problem, blockIndex, coord, 1);
                 }
 
                 if (!problem.solved()) {
-                    currentBlock.rotate_90_cw(3);
+                    currentBlock.rotate_90_cw(1);
                     currentBlock.flatten();
                 }
             }
@@ -127,7 +148,7 @@ public class Solve {
         }
     }
 
-    public static void recurse_3D_branch(Problem problem, int blockIndex, Coordinate coord, boolean skew) {
+    public static void recurse_3D_branch(Problem problem, int blockIndex, Coordinate coord, int transformMode) {
         if (!problem.solved()) {
             problem.caseTested++;
 
@@ -143,10 +164,12 @@ public class Solve {
                 problem.remove_block(blockIndex, coord);
             }
 
-            if (skew) {
-                problem.blocks[blockIndex].skew_90_ccw_ij(1);
-            } else {
+            if (transformMode == 0) {
                 problem.blocks[blockIndex].rotate_90_cw(1);
+            } else if (transformMode == 1) {
+                problem.blocks[blockIndex].skew_90_ccw_ij(1);
+            } else if (transformMode == 2) {
+                problem.blocks[blockIndex].reflect_horizontal();
             }
         }
     }
